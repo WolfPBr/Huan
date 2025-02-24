44be20bb18d85bf8b3b779186a7f1c5db26f3c54
Valder1no <vfersters@gmail.com>
2025-02-24 16:42:21 +0200

    111

--- Assets/Scripts/DestroyProjectile.cs
+++ Assets/Scripts/DestroyProjectile.cs
@@ -5,6 +5,6 @@ public class DestroyProjectile : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Collided with: " + collision.gameObject.name);
-        Destroy(gameObject);
+        Destroy(GameObject);
     }
 }
