38ec519f155bab922d7cf64abe67299f97c683ca
im gonna touch you <martins.babrans7@gmail.com>
2025-03-24 18:10:58 +0200

    pepper pig

--- Assets/scripts/i spent two hours on this bart simpson for nothing.cs
+++ Assets/scripts/i spent two hours on this bart simpson for nothing.cs
@@ -1,54 +1,58 @@
 using UnityEngine;
+using UnityEngine.InputSystem; // For input handling
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class SphereLauncher : MonoBehaviour
+public class ShootObjectOnButtonPress : MonoBehaviour
 {
-    public GameObject cloneSpherePrefab;  // The prefab for the sphere (to be assigned in the Inspector)
-    public float launchForce = 10f;      // The force with which the sphere is launched
-    public float despawnTime = 5f;       // The time (in seconds) after which the sphere will despawn
+    // The object prefab to shoot
+    public GameObject objectToShoot;
 
-    private Camera mainCamera;
+    // The transform from which the object will be shot
+    public Transform shootFromTransform;
 
-    void Start()
+    // The force with which the object will be shot
+    public float shootForce = 10f;
+
+    // The input action that triggers the shooting (this can be mapped in Unity's Input System)
+    public InputActionProperty shootButtonAction;
+
+    private void OnEnable()
     {
-        // Get the main camera
-        mainCamera = Camera.main;
+        // Enable the input action
+        shootButtonAction.action.Enable();
     }
 
-    void Update()
+    private void OnDisable()
     {
-        // Check if the left mouse button is clicked
-        if (Input.GetButtonDown("Fire1")) // 0 is the left mouse button
+        // Disable the input action
+        shootButtonAction.action.Disable();
+    }
+
+    private void Update()
+    {
+        // Check if the button is pressed
+        if (shootButtonAction.action.triggered)
         {
-            LaunchSphere();
+            ShootObject();
         }
     }
 
-    void LaunchSphere()
+    private void ShootObject()
     {
-        // Get the mouse position in the world
-        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        // We don't need the exact hit point for the direction, so we just cast a ray
-        if (Physics.Raycast(ray, out hit))
+        if (objectToShoot && shootFromTransform)
         {
-            // Create the clone sphere at the camera's position
-            GameObject clonedSphere = Instantiate(cloneSpherePrefab, mainCamera.transform.position, Quaternion.identity);
+            // Instantiate the object to shoot at the specified transform
+            GameObject projectile = Instantiate(objectToShoot, shootFromTransform.position, shootFromTransform.rotation);
 
-            // Calculate the direction from the camera to the mouse's position
-            Vector3 direction = (hit.point - mainCamera.transform.position).normalized;
-
-            // Get the Rigidbody component of the cloned sphere
-            Rigidbody rb = clonedSphere.GetComponent<Rigidbody>();
-
+            // Add force to the projectile to make it move (e.g., shoot it forward)
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
             if (rb != null)
             {
-                // Apply a force in the direction of the mouse
-                rb.AddForce(direction * launchForce, ForceMode.VelocityChange);
+                rb.AddForce(shootFromTransform.forward * shootForce, ForceMode.VelocityChange);
             }
 
-            // Destroy the cloned sphere after the specified despawn time
-            Destroy(clonedSphere, despawnTime);
+            // Optional: Destroy the object after a short delay to avoid clutter
+            Destroy(projectile, 5f); // Destroy after 5 seconds
         }
     }
 }
